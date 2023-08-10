0d2a039ba0e2da1df28ce7e901a05fb4b3754f4a
Everton da Rosa <everton3x@gmail.com>
2023-08-10 16:38:22 -0300

    composer.json updated

--- src/Ptk/DataRepo/DataRepo.php
+++ src/Ptk/DataRepo/DataRepo.php
@@ -2,16 +2,15 @@
 
 namespace Ptk\DataRepo;
 
+use Exception;
 use PDO;
 use PDOException;
-use Exception;
-
 use Ptk\DataRepo\Table\Table;
 
 class DataRepo extends PDO {
-    
+
     private array $tables = [];
-    
+
     public function __construct(string $local = ':memory:', ?string $userName = null, ?string $password = null, ?array $options = null) {
         $dsn = "sqlite:$local";
         parent::__construct($dsn, $userName, $password, $options);
@@ -24,25 +23,25 @@ class DataRepo extends PDO {
         $table->setRepo($this);
         return $this;
     }
-    
+
     public function getTable(string $tableName): Table {
         $this->throwIfTableNotExists($tableName);
         return $this->tables[$tableName];
     }
-    
+
     public function tableExists(string $tableName): bool {
         $stmt = $this->prepare('SELECT name FROM sqlite_master WHERE type="table" AND name=:tableName;');
         $stmt->execute(['tableName' => $tableName]);
         $count = sizeof($stmt->fetchAll());
-        if ($count == 0 ) return false;
-        if ($count == 1 ) return true;
+        if ($count == 0)
+            return false;
+        if ($count == 1)
+            return true;
         throw new PDOException("Quantidade de tabelas é maior que 1: $count");
     }
-    
+
     private function throwIfTableNotExists(string $tableName): void {
-        if (!$this->tableExists($tableName)) throw new Exception("Tabela $tableName não existe!");
+        if (!$this->tableExists($tableName))
+            throw new Exception("Tabela $tableName não existe!");
     }
-
-    
-
 }
