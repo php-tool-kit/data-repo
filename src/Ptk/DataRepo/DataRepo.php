c47f1dc47278bfd81c5af7c75871850f0a0301d2
Everton da Rosa <everton3x@gmail.com>
2023-08-10 16:53:25 -0300

    Adicionada docstring

--- src/Ptk/DataRepo/DataRepo.php
+++ src/Ptk/DataRepo/DataRepo.php
@@ -7,16 +7,39 @@ use PDO;
 use PDOException;
 use Ptk\DataRepo\Table\Table;
 
+/**
+ * Repositório de dados correpondendo a um banco de dados do SQLite.
+ * 
+ * @author Everton da Rosa <everton3x@gmail.com>
+ */
 class DataRepo extends PDO {
 
+    /**
+     * 
+     * @var array<Table> Lista das tabelas.
+     */
     private array $tables = [];
 
+    /**
+     * Construtor da classe.
+     * 
+     * @param string $local Local de armazenamento da tabela. O padrão é na memória. Se fornecido um caminho de arquivo, ele será utilizado. Deve ser compatível com o PDO DSN para SQLite.
+     * @param string|null $userName
+     * @param string|null $password
+     * @param array|null $options
+     */
     public function __construct(string $local = ':memory:', ?string $userName = null, ?string $password = null, ?array $options = null) {
         $dsn = "sqlite:$local";
         parent::__construct($dsn, $userName, $password, $options);
         parent::setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     }
 
+    /**
+     * Cria uma nova tabela no repositório.
+     * 
+     * @param Table $table
+     * @return DataRepo
+     */
     public function addTable(Table $table): DataRepo {
         $this->exec($table->getSQL());
         $this->tables[$table->getTableName()] = $table;
@@ -24,11 +47,24 @@ class DataRepo extends PDO {
         return $this;
     }
 
+    /**
+     * Retorna uma tabela específica.
+     * 
+     * @param string $tableName
+     * @return Table
+     */
     public function getTable(string $tableName): Table {
         $this->throwIfTableNotExists($tableName);
         return $this->tables[$tableName];
     }
 
+    /**
+     * Indica se uma tabela existe ou não no repositório.
+     * 
+     * @param string $tableName
+     * @return bool
+     * @throws PDOException
+     */
     public function tableExists(string $tableName): bool {
         $stmt = $this->prepare('SELECT name FROM sqlite_master WHERE type="table" AND name=:tableName;');
         $stmt->execute(['tableName' => $tableName]);
@@ -40,6 +76,15 @@ class DataRepo extends PDO {
         throw new PDOException("Quantidade de tabelas é maior que 1: $count");
     }
 
+    /**
+     * Dispara uma exceção se a tabela não existir.
+     * 
+     * Serve como auxiliar em métodos que precisam falhar se a tabela não existir.
+     * 
+     * @param string $tableName
+     * @return void
+     * @throws Exception
+     */
     private function throwIfTableNotExists(string $tableName): void {
         if (!$this->tableExists($tableName))
             throw new Exception("Tabela $tableName não existe!");
--- src/Ptk/DataRepo/Table/Table.php
+++ src/Ptk/DataRepo/Table/Table.php
@@ -8,37 +8,98 @@ use \Ptk\DataRepo\DataRepo;
 use \Ptk\DataRepo\Field\FieldTypes;
 use \Ptk\DataRepo\Field\FieldCollates;
 
+/**
+ * Representa uma tabela dentro do repositório.
+ * 
+ * @author Everton da Rosa <everton3x@gmail.com>
+ */
 class Table {
     
+    /**
+     * 
+     * @var string Nome da tabela.
+     */
     private string $name;
+    
+    /**
+     * 
+     * @var array<string, mixed> Definição dos campos da tabela.
+     */
     private array $fieldsDef = [];
     
+    /**
+     * 
+     * @var bool A tabela é temporária?
+     */
     private bool $temporary = false;
+    
+    /**
+     * 
+     * @var string Nome do schema no qual a tabela será incluída.
+     */
     private string $schema = 'main';
+    
+    /**
+     * 
+     * @var bool Determina se será ou não usada IF NOT EXISTS ao criar a tabela.
+     */
     private bool $createIfNotExists = true;
     
+    /**
+     * 
+     * @var DataRepo O repositório no qual a tabela está anexada.
+     */
     private DataRepo $repo;
     
-
+    /**
+     * Construtor de classe.
+     * 
+     * @param string $name Nome da tabela. Deve obedecer à conveção de nomenclatura do SQLite.
+     */
     public function __construct(string $name) {
         $this->name = $name;
     }
     
-
+    /**
+     * Define se a tabela é temporária ou não.
+     * @param bool $value
+     * @return Table
+     */
     public function temporaryTable(bool $value): Table {
         $this->temporary = $value;
         return $this;
     }
     
+    /**
+     * Diz se a tabela é temporária ou não.
+     * @return bool
+     */
     public function isTemporary(): bool {
         return $this->temporary;
     }
     
+    /**
+     * Define se a criação será pulada se a tabela já existir.
+     * 
+     * @param bool $value
+     * @return Table
+     */
     public function createIfNotExists(bool $value): Table {
         $this->createIfNotExists = $value;
         return $this;
     }
     
+    /**
+     * Adiciona um novo campo à tabela (apenas antes dela ser criada no repositório).
+     * 
+     * @param string $name Nome do campo.
+     * @param FieldTypes $type Tipo do campo (um dos tipos suportados pelo SQlite).
+     * @param bool $nullable Se o campo pode ser NULL.
+     * @param bool $unique Se o campo é único ou não.
+     * @param mixed $default Valor padrão para o campo.
+     * @param FieldCollates|null $collate Um dos collations suportados pelo SQLite.
+     * @return Table
+     */
     public function addField(string $name, FieldTypes $type = FieldTypes::Text, bool $nullable = true, bool $unique = false, mixed $default = null, ?FieldCollates $collate = null): Table {
         $this->fieldsDef[$name] = [
             'name' => $name,
@@ -51,7 +112,14 @@ class Table {
         return $this;
     }
     
-    protected function buildColDef(): string {
+    /**
+     * Construtor da string de definição de colunas.
+     * 
+     * Método auxiliar para Table::getSQL
+     * 
+     * @return string
+     */
+    private function buildColDef(): string {
         $colDef = [];
         foreach ($this->fieldsDef as $field){
             $notnull = '';
@@ -67,6 +135,11 @@ class Table {
         return implode(', ', $colDef);
     }
 
+    /**
+     * Construtor do código SQL para a criação da tabela.
+     * 
+     * @return string
+     */
     public function getSQL(): string {
         $strTemp = '';
         if($this->temporary) {
@@ -83,10 +156,25 @@ class Table {
         return $sql;
     }
     
+    /**
+     * Método auxiliar para vincular a tabela ao repositório.
+     * 
+     * Geralmente não é preciso utilizar esse método.
+     * 
+     * Ele é ustilizado por DataRepo::addTable
+     * 
+     * @param DataRepo $repo
+     * @return void
+     */
     public function setRepo(DataRepo $repo): void {
         $this->repo = $repo;
     }
 
+    /**
+     * Retorna a lista de nomes de campos da tabela.
+     * 
+     * @return array<string>
+     */
     public function getFieldNames(): array {
         $names = [];
         foreach ($this->fieldsDef as $field){
@@ -95,6 +183,11 @@ class Table {
         return $names;
     }
 
+    /**
+     * Retorna a lista de tipos dos campos da tabela.
+     * 
+     * @return array<FieldTypes>
+     */
     public function getFieldTypes(): array {
         $types = [];
         foreach ($this->fieldsDef as $field){
@@ -103,10 +196,24 @@ class Table {
         return $types;
     }
     
+    /**
+     * Retorna o nome da tabela.
+     * 
+     * @return string
+     */
     public function getTableName(): string {
         return $this->name;
     }
     
+    /**
+     * Insere dados na tabela.
+     * 
+     * Os dados devem estar organizados num array bidimensional por linhas/colunas.
+     * 
+     * @param array<array> $data
+     * @return Table
+     * @throws Exception
+     */
     public function insertData(array $data): Table {
         $strFields = implode(', ', $this->getFieldNames());
         $valueKeys = [];
